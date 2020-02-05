6e4335f17c80a11b96439c3b71c6b46348e07d10
max <maksym.zhuk@ria.com>
2020-02-05 21:32:47 +0200

    add bin file

---
+++ bin/searchCoordinate.js
@@ -0,0 +1,12 @@
+/**
+ * yarn search-inverted accessory
+ * yarn search-inverted inventors
+ * yarn search-inverted accessory AND inventors
+ * yarn search-inverted accessory OR inventors
+ * yarn search-inverted accessory NOT inventors
+ */
+
+const { search } = require('../src/coordinate/coordinateIndexManager');
+
+// eslint-disable-next-line no-console
+console.log(search(...process.argv.slice(2)));
